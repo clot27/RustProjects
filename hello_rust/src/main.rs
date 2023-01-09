a533832d0d8e5324aa81d131ac9daf5b814e2313
Aditya Yadav <adityayadav11082@gmail.com>
2023-01-09 14:37:08 +0530

    intializing new project

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,42 +1,42 @@
 // A program to convert temperature between Fahrenheit and Celsius
 
-// use std::io;
+use std::io;
 fn main() {
-    // println!("Welcome to Fahrenheit and Celsius Calculator!\nPress F to convert from Fahrenheit to Celsius\nPress C to convert from Celsius to Fahrenheit");
-    // let mut choice = String::new();
-    // io::stdin()
-    //     .read_line(&mut choice)
-    //     .expect("Failed to read line");
-    // let choice = choice.trim().to_uppercase();
-    // loop {
-    //     println!("Enter the unit, Q to quit");
-    //     let mut unit = String::new();
-    //     io::stdin()
-    //         .read_line(&mut unit)
-    //         .expect("Failed to read line");
-    //     if unit.trim().to_uppercase() == "Q" {
-    //         println!("Thanks for using me!, will see you later :)");
-    //         break;
-    //     }
-    //     let unit: f64 = unit
-    //         .trim()
-    //         .parse()
-    //         .expect("Please enter a valid float number");
-    //     if choice == "F" {
-    //         let temperature = f_to_c(unit);
-    //         println!("{unit} fahrenheit = {temperature} celsius");
-    //     } else {
-    //         let temperature = c_to_f(unit);
-    //         println!("{unit} celsius = {temperature} fahrenheit");
-    //     }
-    //     continue;
-    // }
+    println!("Welcome to Fahrenheit and Celsius Calculator!\nPress F to convert from Fahrenheit to Celsius\nPress C to convert from Celsius to Fahrenheit");
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+    let choice = choice.trim().to_uppercase();
+    loop {
+        println!("Enter the unit, Q to quit");
+        let mut unit = String::new();
+        io::stdin()
+            .read_line(&mut unit)
+            .expect("Failed to read line");
+        if unit.trim().to_uppercase() == "Q" {
+            println!("Thanks for using me!, will see you later :)");
+            break;
+        }
+        let unit: f64 = unit
+            .trim()
+            .parse()
+            .expect("Please enter a valid float number");
+        if choice == "F" {
+            let temperature = f_to_c(unit);
+            println!("{unit} fahrenheit = {temperature} celsius");
+        } else {
+            let temperature = c_to_f(unit);
+            println!("{unit} celsius = {temperature} fahrenheit");
+        }
+        continue;
+    }
 }
 
-// fn f_to_c(f: f64) -> f64 {
-//     (f - 32.0) * (5.0 / 9.0)
-// }
+fn f_to_c(f: f64) -> f64 {
+    (f - 32.0) * (5.0 / 9.0)
+}
 
-// fn c_to_f(c: f64) -> f64 {
-//     (c * (9.0 / 5.0)) + 32.0
-// }
+fn c_to_f(c: f64) -> f64 {
+    (c * (9.0 / 5.0)) + 32.0
+}
