755a26cc9973eb95f1ee43c753b37ebd8b94c140
Aditya Yadav <adityayadav11082@gmail.com>
2023-01-09 07:51:59 +0530

    first commit

---
+++ hello_rust/target/debug/deps/hello_rust-65277566a33c10fc.d
@@ -0,0 +1,5 @@
+/home/adityay/RustTut/hello_rust/target/debug/deps/hello_rust-65277566a33c10fc.rmeta: src/main.rs
+
+/home/adityay/RustTut/hello_rust/target/debug/deps/hello_rust-65277566a33c10fc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/deps/hello_rust-ec5b6490a6e3c00e.d
@@ -0,0 +1,5 @@
+/home/adityay/RustTut/hello_rust/target/debug/deps/hello_rust-ec5b6490a6e3c00e.rmeta: src/main.rs
+
+/home/adityay/RustTut/hello_rust/target/debug/deps/hello_rust-ec5b6490a6e3c00e.d: src/main.rs
+
+src/main.rs:
